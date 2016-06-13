cae81b7967a51254b051a0f40540dc03327c296c
diegomebro <diegomatias.mb@hotmail.com>
2016-06-13 19:27:43 -0400

    Segunda pregunta

---
+++ P2/main.js
@@ -0,0 +1,12 @@
+/* Crear una función que devuelva una copia de un objeto, si el objeto contiene un arreglo éste también debe ser
+una copia. */
+
+var i = [1,2,3,4];
+
+function copia(parametro){
+	var copia_x = [];
+	for (var y = 0; y < parametro.length; y++){
+		arreglo.ṕush(parametro[y]);
+	}
+	return arreglo
+}
